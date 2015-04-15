2491e93e56db151565f0edd951de311269a20026
Samantha Belkin <samantha.belkin@gmail.com>
2015-04-15 17:34:17 -0400

    add javascript

---
+++ blog/recent_posts.js
@@ -0,0 +1,21 @@
+document.write('\
+<right-column>\
+<h4 class="right">Recent Posts</h4>\
+<ul>\
+  <li><a class="blog-link" href="c7-values.html">W7C: Values</a></li>\
+  <li><a class="blog-link" href="t7-JavaScript.html">W7T: JavaScript vs. Ruby</a></li>\
+  <li><a class="blog-link" href="c6-stereotype-threat.html">W6C: Stereotype Threat</a></li>\
+  <li><a class="blog-link" href="t6-oop-concepts.html">W6T: Blocs, Procs, and Lambdas</a></li>\
+  <li><a class="blog-link" href="c5-feedback.html">W5C: Feedback</a></li>\
+  <li><a class="blog-link" href="t5-ruby-classes.html">W5T: Ruby Classes</a></li>\
+  <li><a class="blog-link" href="c4-tech-issues.html">W4C: Tech Issues</a></li>\
+  <li><a class="blog-link" href="t4-enumerable-methods.html">W4T: Enumerable Map</a></li>\
+  <li><a class="blog-link" href="c3-thinking-style.html">W3C: Thinking Style</a></li>\
+  <li><a class="blog-link" href="t3-arrays-hashes.html">W3T: Ruby Arrays and Hashes</a></li>\
+  <li><a class="blog-link" href="t2-css-design.html">W2T: CSS Technical</a></li>\
+  <li><a class="blog-link" href="c1-chefs-kitchen.html">W1C: Chef\'s Kitchen</a></li>\
+  <li><a class="blog-link" href="t1-git-blog.html">W1T: Git Bloggin\'</a></li>\
+</ul>\
+</right-column>\
+\
+  ')
