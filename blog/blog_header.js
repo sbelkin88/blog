fc45b57b26cbbc292cf663748ee65b3d714d6c70
Samantha Belkin <samantha.belkin@gmail.com>
2015-04-15 17:44:32 -0400

    add javascript

---
+++ blog/blog_header.js
@@ -0,0 +1,39 @@
+document.write('\
+<section class="top-bar">\
+<section class="header" id="envelope">\
+  <a href="mailto:samantha.belkin@gmail.com"><img src="https://github.com/sbelkin88/sbelkin88.github.io/blob/master/Envelope.png?raw=true"/></a>\
+</section>\
+<div class="header">\
+  &bull;\
+</div>\
+<section class="header">\
+  <a href="../about.html">ABOUT</a>\
+</section>\
+<div class="header">\
+  &bull;\
+</div>\
+<section class="header">\
+  <a href="blog-index.html">BLOG</a>\
+</section>\
+<div class="header">\
+  &bull;\
+</div>\
+<section class="header">\
+  <a href="../projects.html">PROJECTS</a>\
+</section>\
+<div class="header">\
+  &bull;\
+</div>\
+<section class="header">\
+  <a href="../contact.html">CONTACT</a>\
+</section>\
+</section>\
+<h1>\
+  <a href="../index.html">SAMANTHA SARAH BELKIN</a>\
+</h1>\
+<h2>\
+  contemplate, wish, wonder\
+</h2>\
+<hr id="line-break">\
+\
+  ');
